1b5716e93ca1321cab4a814ba3f3ca27ae1a33f9
cksagar <cksagar7@gmail.com>
2025-04-07 17:51:11 +0530

    added api url to frontend constants

--- client/src/utils/constants.jsx
+++ client/src/utils/constants.jsx
@@ -1,7 +1,7 @@
 export const LOGO_URL =
   "https://static.vecteezy.com/system/resources/previews/009/944/948/non_2x/food-company-logo-design-free-vector.jpg";
 
-export const BASE_URL = "https://your-json-server.onrender.com/api";
+export const BASE_URL = "https://react-food-app-cz9v.onrender.com/api";
 
 export const API_URLS = {
   RESTAURANTS_URL: "/restaurants",
