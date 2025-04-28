5b47b7a13ae028e9de8c850353b577237edbc7b6
cksagar <cksagar7@gmail.com>
2025-04-28 15:05:20 +0530

    fix(constants): update BASE_URL and LOCAL_BASE_URL to include API versioning

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,9 +1,9 @@
 export const LOGO_URL =
   "https://static.vecteezy.com/system/resources/previews/009/944/948/non_2x/food-company-logo-design-free-vector.jpg";
 
-export const BASE_URL = "https://nest-demo-mgvs.onrender.com";
+export const BASE_URL = "https://nest-demo-mgvs.onrender.com/api/v1";
 
-export const LOCAL_BASE_URL = "http://localhost:3000";
+export const LOCAL_BASE_URL = "http://localhost:3000/api/v1";
 
 export const API_URLS = {
   RESTAURANTS_URL: "/restaurant",
